f00e1fcad3800089913dfba0aa2840444e41d299
106122241+KatieUmbra@users.noreply.github.com <Katherine@kaytea.dev>
2023-08-15 14:07:52 -0500

    Metadata

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { MetaTags } from "svelte-meta-tags";
+</script>
+
+<MetaTags
+	title= "Pretty girl - Search"
+	description = "Kanwi's (Aka. Katherine) presentation carrd with useful links, likes, etc..."
+	openGraph={{
+		type: "website",
+		url: "https://carrd.kaytea.dev",
+		title: "Pretty girl - Search",
+		description: "Kanwi's (Aka. Katherine) presentation carrd with useful links, likes, etc...",
+		site_name: "Kanwi's Carrd",
+		images : [
+			{
+				url: "../lib/static/preview.png",
+				width: 1200,
+				height: 627,
+				alt: "Preview of the website"
+			}
+		]
+	}}
+/>
